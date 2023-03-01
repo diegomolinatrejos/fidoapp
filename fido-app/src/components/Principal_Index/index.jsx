52a758f48bd57cfb3d16b073495fefb80c1c9c5c
Keylor Gómez Rodríguez <kgomezr@ucenfotec.ac.cr>
2023-03-01 17:18:18 -0600

    Creación del footer de la aplicación

---
+++ fido-app/src/components/Principal_Index/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+import Footer from '../Footer'
+
+
+function Principal_Index() {
+  return (
+    <Footer/>
+  )
+}
+
+export default Principal_Index
