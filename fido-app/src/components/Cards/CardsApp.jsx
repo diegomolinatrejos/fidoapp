d958f867ccacf34db257b0908808d639d4d44edc
Keylor Gómez Rodríguez <kgomezr@ucenfotec.ac.cr>
2023-03-20 15:18:10 -0600

    Se solucionan errores de estilos

--- fido-app/src/components/Cards/CardsApp.jsx
+++ fido-app/src/components/Cards/CardsApp.jsx
@@ -8,23 +8,23 @@ function CardsApp() {
   return (
     <React.Fragment>
         <div style={{backgroundColor:'#75E3EA'}} className='PrincipalContainer'>
-            <h2 style={{fontSize:'1.3rem'}}>Ofertas y promociones</h2>
+            <h2 style={{fontSize:'1.3rem', margin:'1rem', fontWeight:'bold'}}>Ofertas y promociones</h2>
             <CardList list={listPromos}/>
         </div>
         <div  className='PrincipalContainer' style={{backgroundColor:'#fff'}}>
-            <h2 style={{fontSize:'1.3rem'}}>Hoteles</h2>
+            <h2 style={{fontSize:'1.3rem', margin:'1rem', fontWeight:'bold'}}>Hoteles</h2>
             <CardList list={listHotels}/>
         </div>
         <div  className='PrincipalContainer' style={{backgroundColor:'#FFC278'}}>
-            <h2 style={{fontSize:'1.3rem'}}>Entrenamiento</h2>
+            <h2 style={{fontSize:'1.3rem', margin:'1rem', fontWeight:'bold'}}>Entrenamiento</h2>
             <CardList list={listEntrenamiento}/>
         </div>
         <div  className='PrincipalContainer' style={{backgroundColor:'#fff'}}>
-        <h2 style={{fontSize:'1.3rem'}}>Grooming</h2>
+        <h2 style={{fontSize:'1.3rem', margin:'1rem', fontWeight:'bold'}}>Grooming</h2>
             <CardList list={listGrooming}/>
         </div>
         <div  className='PrincipalContainer' style={{backgroundColor:'#F08080'}}>
-            <h2 style={{fontSize:'1.3rem'}}>Veterinaria</h2>
+            <h2 style={{fontSize:'1.3rem', margin:'1rem', fontWeight:'bold'}}>Veterinaria</h2>
             <CardList list={listVeterinaria}/>
         </div>
     </React.Fragment>
